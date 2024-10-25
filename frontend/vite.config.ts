c7fc7b22a96c84dd1e5b4f0c917bd34b0b406749
Felipe Beiger <beigerfelipe@gmail.com>
2024-10-25 10:17:49 -0300

    chore: add absoolute paths to tsconfig and vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@pages': path.resolve(__dirname, './src/presentation/pages'),
       '@components': path.resolve(__dirname, './src/components'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@data': path.resolve(__dirname, './src/data'),
