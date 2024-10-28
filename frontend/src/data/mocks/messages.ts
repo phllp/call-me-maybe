a748077f0863cd43bfbfd552f799767bda5b874f
Felipe Beiger <beigerfelipe@gmail.com>
2024-10-28 12:24:36 -0300

    chore: create data mocks

---
+++ frontend/src/data/mocks/messages.ts
@@ -0,0 +1,46 @@
+import { ChatMessage } from '@core/entities/chat-message';
+
+const chatMessages: ChatMessage[] = [
+  {
+    text: 'Olá, tudo bem?',
+    sender: 'me',
+  },
+  {
+    text: 'Oi, tudo bem sim e você?',
+    sender: 'them',
+  },
+  {
+    text: 'Tudo ótimo, obrigado por perguntar!',
+    sender: 'me',
+  },
+  {
+    text: 'Que bom, fico feliz em saber!',
+    sender: 'them',
+  },
+  {
+    text: 'Então, o que você está fazendo agora?',
+    sender: 'me',
+  },
+  {
+    text: 'Estou assistindo um filme, e você?',
+    sender: 'them',
+  },
+  {
+    text: 'Estou trabalhando em um projeto pessoal, mas estou com dificuldades.',
+    sender: 'me',
+  },
+  {
+    text: 'Posso te ajudar em algo?',
+    sender: 'them',
+  },
+  {
+    text: 'Seria ótimo, obrigado!',
+    sender: 'me',
+  },
+  {
+    text: 'Vamos lá, me conte mais sobre o projeto.',
+    sender: 'them',
+  },
+];
+
+export default chatMessages;
---
+++ frontend/src/data/mocks/users.ts
@@ -0,0 +1,31 @@
+import { UserData } from '@core/entities/user';
+
+const mockedUsers: UserData[] = [
+  {
+    id: '1',
+    name: 'Eleanor Rigby',
+    email: 'eleanor.rigby@er.com',
+  },
+  {
+    id: '2',
+    name: 'Father McKenzie',
+    email: 'father.mckenzie@fm.com',
+  },
+  {
+    id: '3',
+    name: 'Lucy in the Sky with Diamonds',
+    email: 'lucy@lsd.com',
+  },
+  {
+    id: '4',
+    name: 'Bungalow Bill',
+    email: 'bungallow.bill@bb.com',
+  },
+  {
+    id: '5',
+    name: 'Mr. Kite',
+    email: 'mr.kite@mk.com',
+  },
+];
+
+export default mockedUsers;
