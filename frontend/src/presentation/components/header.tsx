7158dccccc264c0c1788a14d8559698b263741fb
Felipe Beiger <beigerfelipe@gmail.com>
2024-10-28 16:58:07 -0300

    feat: add Invite btn to Header

--- frontend/src/presentation/components/header.tsx
+++ frontend/src/presentation/components/header.tsx
@@ -1,22 +1,13 @@
-import Button from './generic/button';
-import { MessageCirclePlus } from 'lucide-react';
+import InviteLink from './invitation-link-modal';
 
 const Header: React.FC = () => {
-  const handleInvite = () => {
-    console.log('Invite');
-  };
-
   return (
     <nav className="flex items-center justify-between bg-rose-950">
       <a href="/home" className="ml-12">
         <p className="text-rose-100 text-xl">Call Me Maybe</p>
       </a>
       <div className="actionsContainer mr-12">
-        <Button
-          onClick={handleInvite}
-          name="Invite"
-          icon={<MessageCirclePlus />}
-        />
+        <InviteLink />
       </div>
     </nav>
   );
