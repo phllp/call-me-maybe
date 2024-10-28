92a825360ea02420d3ce245a04ac07346d06df9b
Felipe Beiger <beigerfelipe@gmail.com>
2024-10-28 17:13:12 -0300

    feat: update invite-link modal style

--- frontend/src/presentation/components/invitation-link-modal.tsx
+++ frontend/src/presentation/components/invitation-link-modal.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { useEffect, useState } from 'react';
-import { CheckCheck, Copy, MessageCirclePlus } from 'lucide-react';
+import { MessageCirclePlus } from 'lucide-react';
 import api from '@external/axios';
 
 export default function InviteLink() {
@@ -8,7 +8,6 @@ export default function InviteLink() {
   const [inviteLink, setInviteLink] = useState('');
 
   const [success, setSuccess] = useState(false);
-  const [copyText, setCopyText] = useState('Copy');
 
   useEffect(() => {
     api.get('/invite-link').then((response) => {
@@ -19,10 +18,8 @@ export default function InviteLink() {
   const copyToClipboard = () => {
     navigator.clipboard.writeText(inviteLink);
     setSuccess(true);
-    setCopyText('Copied!');
     setTimeout(() => {
       setSuccess(false);
-      setCopyText('Copy');
     }, 2000);
   };
 
@@ -37,26 +34,24 @@ export default function InviteLink() {
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/55" />
         <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-rose-200 text-slate-900 p-8 shadow-lg rounded-md">
-          <Dialog.Title className="text-2xl font-medium">
+          <Dialog.Title className="text-2xl font-medium text-rose-900">
             Invite Someone
           </Dialog.Title>
-          <div className="mt-10">
+          <div className="my-10 text-rose-950">
             Share the following link to someone you want to talk with.
           </div>
-          <div
-            className="relative group bg-rose-50 p-2 rounded-sm mt-4 cursor-pointer flex justify-between items-center"
-            onClick={copyToClipboard}
-          >
-            <a>{inviteLink}</a>
-            {success ? (
-              <CheckCheck className="text-green-800" />
-            ) : (
-              <Copy className="text-rose-800" />
-            )}
-
-            <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity">
-              {copyText}
-            </span>
+          <div className="flex justify-center items-center flex-col pb-2">
+            <button
+              className="border-2 border-rose-900 text-rose-900 bg-rose-200 py-2 px-4 rounded-md hover:bg-rose-900 hover:text-rose-200 flex items-center justify-center gap-2"
+              onClick={copyToClipboard}
+            >
+              <p className="font-medium">Copy Link</p>
+            </button>
+            <div className="h-4">
+              <p className={`mt-4 text-rose-900 ${success ? '' : 'hidden'}`}>
+                Invitation link copied...
+              </p>
+            </div>
           </div>
         </Dialog.Content>
       </Dialog.Portal>
