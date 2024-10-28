31deea8d40b436b29021b60aa42ec330c43a25d2
Felipe Beiger <beigerfelipe@gmail.com>
2024-10-28 13:50:33 -0300

    feat: create invite-link dialog

---
+++ frontend/src/presentation/components/invitation-link-modal.tsx
@@ -0,0 +1,59 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { useState } from 'react';
+import { CheckCheck, Copy, MessageCirclePlus } from 'lucide-react';
+
+export default function InviteLink() {
+  const [open, setOpen] = useState(false);
+
+  const [success, setSuccess] = useState(false);
+  const [copyText, setCopyText] = useState('Copy');
+
+  const link = 'https://very-serious-link.com/31e3131231dfknsdf';
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(link);
+    setSuccess(true);
+    setCopyText('Copied!');
+    setTimeout(() => {
+      setSuccess(false);
+      setCopyText('Copy');
+    }, 2000);
+  };
+
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger className="text-cmPurple-500">
+        <button className=" text-rose-900 bg-rose-200 py-2 px-4 rounded-md hover:bg-rose-300 hover:text-rose-800 flex items-center justify-center gap-2">
+          <p className="font-medium">Invite</p>
+          <MessageCirclePlus />
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/55" />
+        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-rose-200 text-slate-900 p-8 shadow-lg rounded-md">
+          <Dialog.Title className="text-2xl font-medium">
+            Invite Someone
+          </Dialog.Title>
+          <div className="mt-10">
+            Share the following link to someone you want to talk with.
+          </div>
+          <div
+            className="relative group bg-rose-50 p-2 rounded-sm mt-4 cursor-pointer flex justify-between items-center"
+            onClick={copyToClipboard}
+          >
+            <a>{link}</a>
+            {success ? (
+              <CheckCheck className="text-green-800" />
+            ) : (
+              <Copy className="text-rose-800" />
+            )}
+
+            <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              {copyText}
+            </span>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
