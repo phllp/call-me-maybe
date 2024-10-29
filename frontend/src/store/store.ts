40619ce804b4735968a1f596ed3d565eece9ac0f
Felipe Beiger <beigerfelipe@gmail.com>
2024-10-29 14:33:14 -0300

    chore: configure redux store to pass objects as serializables

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counter-slice';
+import {
+  configureStore,
+  createSerializableStateInvariantMiddleware,
+  isPlain,
+  Tuple,
+} from '@reduxjs/toolkit';
+import counterReducer from '@store/features/counter/counter-slice';
 import callStatusReducer from '@store/features/call-status/call-status-slice';
+
+const isSerializable = (value: unknown) =>
+  value instanceof MediaStream ||
+  isPlain(value) ||
+  value instanceof RTCPeerConnection ||
+  value instanceof RTCSessionDescription;
+
+const serializableMiddleware = createSerializableStateInvariantMiddleware({
+  isSerializable,
+});
 
 // this uses the combineReducer under the hood so we can access our reducer throught state.counter
 export const store = configureStore({
-  reducer: { counter: counterReducer, callStatus: callStatusReducer },
+  reducer: {
+    counter: counterReducer,
+    callStatus: callStatusReducer,
+  },
+  // todo: temporary solution. (not)
+  middleware: () => new Tuple(serializableMiddleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
