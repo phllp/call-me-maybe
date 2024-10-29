a5285b256287d8052b77c3dc7024dfa713bc7c8e
Felipe Beiger <beigerfelipe@gmail.com>
2024-10-28 21:34:46 -0300

    feat: create socketio server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { httpsServer } from './server';
+import { httpsServer, app } from './server';
+import socketServer from './socket-server';
 
+socketServer(httpsServer, app);
 const port = process.env.PORT;
 httpsServer.listen(port, () => {
   console.log(`HTTPS server listening on port ${port}`);
---
+++ backend/src/socket-server.ts
@@ -0,0 +1,14 @@
+import { Server as SocketIOServer } from 'socket.io';
+
+export default (httpsServer: any, app: any) => {
+  const io = new SocketIOServer(httpsServer, {
+    cors: {
+      origin: ['https://localhost:5173', 'http://localhost:5173'],
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`:: Socket Connected ${socket.id} ::`);
+  });
+};
