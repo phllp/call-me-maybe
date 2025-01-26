08796bae73445bf38de1056df016ba80fc73d8de
phllp <felipebeiger@gmail.com>
2025-01-26 10:35:47 -0300

    refactor: update header to redirect to home

--- frontend/src/presentation/components/header.tsx
+++ frontend/src/presentation/components/header.tsx
@@ -3,7 +3,7 @@ import NewSession from './new-session-modal';
 const Header: React.FC = () => {
   return (
     <nav className="flex items-center justify-between bg-rose-950">
-      <a href="/home" className="ml-12">
+      <a href="/" className="ml-12">
         <p className="text-rose-100 text-xl">Call Me Maybe</p>
       </a>
       <div className="actionsContainer mr-12 flex gap-4">
