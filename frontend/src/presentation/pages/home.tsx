5fd24d3e1e9254273e89440b3f9ac4f665d50788
phllp <felipebeiger@gmail.com>
2025-01-26 11:52:27 -0300

    feat: update home component

--- frontend/src/presentation/pages/home.tsx
+++ frontend/src/presentation/pages/home.tsx
@@ -1,7 +1,10 @@
 const Home: React.FC = () => {
   return (
-    <div className="flex">
-      <p>Home</p>
+    <div className="flex flex-1 items-center justify-center">
+      <div className="w-[400px] border border-red-600 bg-purple-400 p-5 rounded-lg">
+        Welcome to Call Me Maybe, a web conference app inspired by the legacy of
+        Carly Rae Jepsen
+      </div>
     </div>
   );
 };
